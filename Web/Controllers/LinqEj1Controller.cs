46c4b25c46869516ef043e23aeaedfe101b2d0b2
alanmoex <alan.moex@gmail.com>
2024-04-03 17:20:33 -0300

    Created LinqEj1Controller

---
+++ Web/Controllers/LinqEj1Controller.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LinqEj1Controller : ControllerBase
+    {
+        [HttpGet()]
+        public int[] Get([FromQuery] int[] inputList)
+        {
+
+            var numQuery =
+                from num in inputList
+                where num > 30 && num < 100
+                select num;
+
+            return numQuery.ToArray();
+        }
+    }
+}
