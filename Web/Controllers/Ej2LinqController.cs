7540298c45220e7859104eec1a681ff27c7e2af6
Lucaslovera24 <lucaslovera24@hotmail.com>
2024-04-11 18:23:01 -0300

    Ejercicio 2 Linq terminado

---
+++ Web/Controllers/Ej2LinqController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej2LinqController : ControllerBase
+    {
+        [HttpGet()]
+        public string[] Get([FromQuery] List<string> words)
+        {
+            var query =
+                from w in words
+                where w.Length > 4
+                select w.ToUpper();
+
+            return query.ToArray();
+        }       
+    }
+}
