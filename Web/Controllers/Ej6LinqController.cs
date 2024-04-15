f047a49e27c6fd4f24ac7acb99a74a372bae6c60
Pedro Ortiz Gasparini <pedrogasparini99@gmail.com>
2024-04-15 18:09:12 -0300

    Ejercicio 6 LINQ

---
+++ Web/Controllers/Ej6LinqController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej6LinqController : ControllerBase
+    {
+        [HttpGet()]
+        public List<string> Get([FromQuery] List<string> words)
+        {
+
+            var query =
+                from n in words
+                select n.Replace("ia", "*");
+
+            return query.ToList();
+        }
+    }
+}
