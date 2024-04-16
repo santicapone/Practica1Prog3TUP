b4c701f52fd0b31c068c0e23937250b66f370e0d
santicapone <santicapone@gmail.com>
2024-04-16 18:25:09 -0300

    Ej3linq

---
+++ Web/Controllers/Ej3linqController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej3linqController : ControllerBase
+    {
+        [HttpGet]
+        public string[] Get([FromQuery] string[] words)
+        {
+            var query =
+                from w in words
+                where w.Substring(0, 1).ToLower() == "b" && w.Substring(1, 1).ToLower() == "r"
+                select w;
+            return query.ToArray();  
+        }
+    }
+}
