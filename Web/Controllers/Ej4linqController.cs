167e08e4429935fc5235de8851be5d65840393b2
santicapone <santicapone@gmail.com>
2024-04-16 19:32:28 -0300

    Ej4linq

---
+++ Web/Controllers/Ej4linqController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej4linqController : ControllerBase
+    {
+        [HttpGet]
+        public List<int> Get([FromQuery] List<int> num)
+        {
+           var query = 
+                (from n in num
+                where n > 0
+                select n).ToList();
+                return query;
+        }
+    }
+}
