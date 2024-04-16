3571efa422bc3e2f448ecc6193d505e75f66af5a
santicapone <santicapone@gmail.com>
2024-04-16 19:47:55 -0300

    Ej5linq

---
+++ Web/Controllers/Ej5linqController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej5linqController : Controller
+    {
+        [HttpGet]
+        public List<string> Get([FromQuery]List<int> num)
+        {
+            
+            var query = 
+                from n in num
+                where n*n > 20
+                select $"{n} | {n*n}";    
+            return query.ToList();
+        }
+    }
+}
