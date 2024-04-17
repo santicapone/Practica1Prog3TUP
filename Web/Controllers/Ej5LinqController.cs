0b9d483252409201736da26129aa811f304be1c4
JavierTonini <javiertonini97@gmail.com>
2024-04-17 13:45:59 -0300

    Ej5Linq

---
+++ Web/Controllers/Ej5LinqController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej5LinqController : ControllerBase
+    {
+        [HttpGet]
+
+        public List<string> Get([FromQuery] List<int> lista)
+        {
+
+            var query = from int l in lista
+
+                        let cuadrado = l * l
+                       
+                        where  cuadrado > 20
+
+                        select $"{l} - {cuadrado}";
+
+            return query.ToList();
+        }
+    }
+}
